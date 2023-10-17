572e57e4d51e6cc46d84d70a26f1b90ae9bf81ba
Laura Lobo <lauralobo364@gmail.com>
2023-10-17 09:18:44 -0300

    Pilha/Stack

--- 10_17/Stack.h
+++ 10_17/Stack.h
@@ -1,5 +1,11 @@
+// PILHA (pensar como uma lista encadeada) 
+// mas se for tamanho fixo usa array
+// (F.I F.O) PRIMEIRO A ENTRAR, PRIMEIRO A SAIR
+// pop (tirar) push(colocar)
 #ifndef STACK_H
 #define STACK_H
+#include <initializer_list>
+#include <stdexcept>
 
 template <typename Type>
 struct Node {
@@ -26,18 +32,63 @@ private:
 public:
     // Construtor default: criar pilha vazia
     Stack() = default;
+
+    // Construtor que recebe uma lista de elementos inicial
+    Stack(std::initializer_list<Type> list) {
+        for(Type elem : list) {
+            push(elem);
+        }
+        /*for(auto it = list.begin(); it != list.end(); ++it) {
+            push(*it);
+        }*/
+    }
     
     // Funçao que insere um elemento no topo da pilha
     void push(const Type& val) {
-        
+        m_top = new Node<Type>(val, m_top);
+        m_size++;
+    }
+
+    // Remover um elemento do topo da pilha
+    // Se a pilha for vazia não faz nada
+    void pop() {
+        if(m_top != nullptr) {
+            Node<Type>* aux = m_top; //cria um aux pro top
+            m_top = m_top->next; //top recebe o proximo
+            aux->next = nullptr; // faz o prox do aux apontar pra null pq se nao perde o resto
+            delete aux; //deleta aux
+            m_size--;
+        }
+    }
+
+    // Funcao que retorna se a pilha esta vazia
+    bool empty() const { // ela ta vazia qnd o m_size é 0 ou top é nullptr
+        return m_top == nullptr;
+    }
+
+    //Retorna o valor do elemento no topo
+    Type& top() {
+        if(empty()) {
+            throw std::out_of_range("empty stack");
+        }
+        return m_top->value;
+    }
+    const Type& top() const { // const  = qnd vc sabe que não vai mudar
+        if(empty()) {
+            throw std::out_of_range("empty stack");
+        }
+        return m_top->value;
+    }
+
+    // Retorna o numero de elementos da pilha
+    unsigned size() const {
+        return m_size;
+    }
+
+    //Destrutor
+    ~Stack() {
+        delete m_top;
     }
 };
-
-
-
-
-
-
-
 
 #endif
--- 10_17/main.cpp
+++ 10_17/main.cpp
@@ -1,5 +1,17 @@
-// PILHA (pensar como uma lista encadeada) 
-// mas se for tamanho fixo usa array
-// (F.I F.O) PRIMEIRO A ENTRAR, PRIMEIRO A SAIR
-// pop (tirar) push(colocar)
+#include <iostream>
+#include "Stack.h"
+using namespace std;
 
+struct Pessoa {
+    int idade;
+    string nome;
+};
+
+int main() {
+    Stack<Pessoa> pilha{{21, "João"}, {20, "Maria"}};
+
+    while(!pilha.empty()) {
+        cout << pilha.top().nome << endl;
+        pilha.pop();
+    }
+}
